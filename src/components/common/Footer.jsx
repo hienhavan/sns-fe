705dc0b57a8da90b63637e291c33e937ca5cc4b3
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-10-04 09:50:10 +0700

    update form login register

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,116 +1,45 @@
-import React from 'react';
-
 function Footer() {
-
-
-
-
+  const titles = [
+    'Social Media',
+    'Giới thiệu',
+    'Blog',
+    'Việc làm',
+    'Trợ giúp',
+    'API',
+    'Quyền riêng tư',
+    'Điều khoản',
+    'Vị trí',
+    'Social Lite',
+    'Threads',
+    'Tải thông tin người liên hệ lên & người không phải người dùng',
+    'Meta đã xác minh',
+  ];
   return (
     <footer className=" mx-auto px-4  bg-blue-100 flex justify-center ">
       <div className="my-5">
         <div className="container">
           <section className="mb-4">
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Social Media
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Giới thiệu         
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-            Blog            
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Việc làm
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Trợ giúp           
-              </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              API          
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Quyền riêng tư          
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Điều khoản          
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Vị trí          
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Social Lite          
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Threads          
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Tải thông tin người liên hệ lên & người không phải người dùng          
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
-              href="#!"
-              role="button"
-            >
-              Meta đã xác minh          
-            </a>
+            {titles.map((title, i) => {
+              return (
+                <a
+                  key={i}
+                  className="btn btn-link btn-floating btn-lg text-gray-800 m-1 text-sm"
+                  href="#!"
+                  role="button"
+                >
+                  {title}
+                </a>
+              );
+            })}
           </section>
-        <div className="text-center text-sm text-gray-700">
-          © 2024 Social:{' '}
-          <a className="text-gray-800" href="https://mdbootstrap.com/">
-          Social.com
-          </a>
+          <div className="text-center text-sm text-gray-700">
+            © 2024 Social:{' '}
+            <a className="text-gray-800" href="https://mdbootstrap.com/">
+              Social.com
+            </a>
+          </div>
         </div>
       </div>
-    </div>
-      
     </footer>
   );
 }
--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,91 +1,105 @@
-import loginService from "../../services/login";
-import Footer from "../common/Footer";
-import { Link, useNavigate } from "react-router-dom";
+// import loginService from "../../services/login";
+import Footer from '../common/Footer';
+import { Link, useNavigate } from 'react-router-dom';
 import { object, string } from 'yup';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
-
 export default function Login() {
-    const [errors, setErrors] = useState({});
-    const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
 
-    const loginSchema = object({
-      email: string().email('Email không hợp lệ').required('Vui lòng nhập email'),
-      password: string().min(6, 'Mật khẩu phải có ít nhất 6 ký tự').required('Vui lòng nhập mật khẩu')
-    });
+  const loginSchema = object({
+    email: string().email('Email không hợp lệ').required('Vui lòng nhập email'),
+    password: string()
+      .min(6, 'Mật khẩu phải có ít nhất 6 ký tự')
+      .required('Vui lòng nhập mật khẩu'),
+  });
 
-    const handleLogin = async (e) => { 
-        e.preventDefault();
-        
-        const formData = {
-          email: e.target[0].value,
-          password: e.target[1].value
-        };
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-        try {
-            await loginSchema.validate(formData, { abortEarly: false });
-            
-            await loginService.login(formData);
-
-            navigate('/')
-        } catch (validationErrors) {
-          if (validationErrors.inner) {
-            const errorMessages = validationErrors.inner.reduce((acc, error) => {
-                return { ...acc, [error.path]: error.message };
-            }, {});
-            setErrors(errorMessages);
-        } else {
-            alert('Login failed, please check your credentials.');
-        }
-      }
+    const formData = {
+      email: e.target[0].value,
+      password: e.target[1].value,
     };
 
-    return (
+    try {
+      await loginSchema.validate(formData, { abortEarly: false });
+
+      // await loginService.login(formData);
+
+      navigate('/');
+    } catch (validationErrors) {
+      if (validationErrors.inner) {
+        const errorMessages = validationErrors.inner.reduce((acc, error) => {
+          return { ...acc, [error.path]: error.message };
+        }, {});
+        setErrors(errorMessages);
+      } else {
+        alert('Login failed, please check your credentials.');
+      }
+    }
+  };
+
+  return (
     <>
-      <div className="flex items-center justify-center h-screen  bg-blue-100" >
+      <div className="flex items-center justify-center h-screen  bg-blue-100">
         <div className="flex flex-col lg:flex-row items-center space-x-6 w-8/12">
           <div className="mb-8 lg:mb-0 text-center lg:text-left">
-            <h1 className="text-4xl lg:text-5xl font-bold text-customGray">Social media</h1>
+            <h1 className="text-4xl lg:text-5xl font-bold text-customGray">
+              Social media
+            </h1>
             <p className="mt-4 text-lg lg:text-xl font-medium text-gray-700">
               Connect with friends and the world around you on Social.
             </p>
           </div>
-      
-          <form onSubmit={(e) => handleLogin(e)} className="bg-white p-8 shadow-lg rounded-lg w-full max-w-md">
+
+          <form
+            onSubmit={(e) => handleLogin(e)}
+            className="bg-white p-8 shadow-lg rounded-lg w-full max-w-md"
+          >
             <input
               type="text"
               placeholder="Email"
-              className={`w-full mb-4 p-3 border ${errors.email ? 'border-red-500' : 'border-gray-300'} rounded focus:outline-none focus:ring-2 focus:ring-blue-500`}
+              className={`w-full mb-4 p-3 border ${
+                errors.email ? 'border-red-500' : 'border-gray-300'
+              } rounded focus:outline-none focus:ring-2 focus:ring-blue-500`}
             />
-            {errors.email && <p className="text-red-500 text-sm mb-2">{errors.email}</p>}
-            
+            {errors.email && (
+              <p className="text-red-500 text-sm mb-2">{errors.email}</p>
+            )}
+
             <input
               type="password"
               placeholder="Password"
-              className={`w-full mb-4 p-3 border ${errors.password ? 'border-red-500' : 'border-gray-300'} rounded focus:outline-none focus:ring-2 focus:ring-blue-500`}
+              className={`w-full mb-4 p-3 border ${
+                errors.password ? 'border-red-500' : 'border-gray-300'
+              } rounded focus:outline-none focus:ring-2 focus:ring-blue-500`}
             />
-            {errors.password && <p className="text-red-500 text-sm mb-2">{errors.password}</p>}
-            
+            {errors.password && (
+              <p className="text-red-500 text-sm mb-2">{errors.password}</p>
+            )}
+
             <button
               type="submit"
               className="w-full bg-customGray text-white py-3 rounded-lg font-semibold hover:bg-blue-500 transition duration-300"
             >
               Log In
             </button>
-            
+
             <a href="#" className="block mt-4 text-blue-600 text-center">
               Forgot Password?
             </a>
-            
+
             <hr className="my-6 border-gray-200" />
-            
+
             <Link
-            to={'/register'}
-            className="w-full bg-green-600 text-white py-3 rounded-lg font-semibold hover:bg-green-500 transition duration-300 text-center block"
+              to={'/register'}
+              className="w-full bg-green-600 text-white py-3 rounded-lg font-semibold hover:bg-green-500 transition duration-300 text-center block"
             >
-            Create New Account
+              Create New Account
             </Link>
             <div className="flex items-center justify-center my-6">
               <hr className="w-10 border-gray-300" />
@@ -107,7 +121,7 @@ export default function Login() {
           </form>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
-    );
+  );
 }
