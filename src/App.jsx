61c9ac7c0c3bec05513a47b409753f1413b7dad0
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-10-07 08:31:43 +0700

    view header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import './App.css'
-import {Outlet, Route, Routes} from 'react-router-dom'
-import LoginForm from './components/login/LoginForm'
-import RegisterForm from './components/login/RegisterForm'
+import './App.css';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import LoginForm from './components/login/LoginForm';
+import RegisterForm from './components/login/RegisterForm';
+import Home from './components/home/Home';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 
 function App() {
-
   return (
     <>
-        <Routes>
-          <Route path={'/login'} element={<LoginForm/>}></Route>
-          <Route path={'/register'} element={<RegisterForm/>}></Route>
-        </Routes>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+    <Footer />
+  </>
+);
+
+export default App;
--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,80 @@
-import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faUser,
+  faPencilAlt,
+  faPowerOff,
+} from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="relative fix top-0 left-0 z-50 w-full">
+      <div className="bg-[#34465d] h-[66px] px-4 shadow-[0px_0px_5px_rgba(0,0,0,0.2)] flex items-center fixed">
+        <div className="logo relative z-10 w-[10%] mr-9 ">
+          <a title="" href="newsfeed.html">
+            <img src="/public/logo_img.png" alt="logo" />
+          </a>
+        </div>
+        <div className="flex items-center w-[150vh]">
+          <div className="top-search relative w-full ">
+            <form method="post" className="flex">
+              <input
+                type="text"
+                placeholder="Search People, Pages, Groups etc"
+                className="border-collapse border-gray-400 placeholder-gray-400 text-[15px] p-[9px_18px] w-[80%] rounded-[30px] bg-[#49586e] focus:outline-none focus:border-transparent text-white"
+              />
+              <button className="absolute right-2 top-2 bg-transparent border-none text-gray-400">
+                <i className="ti-search"></i>
+              </button>
+            </form>
+          </div>
+
+          <div className="user-img relative cursor-pointer leading-[65px] flex w-[20%] justify-center items-center group">
+            <h5 className="text-white inline-block text-[14px] font-medium mr-2">
+              Jack Carter
+            </h5>
+            <img
+              src="/public/avatar.jpg"
+              alt=""
+              className="rounded-full w-[50px] h-[50px] border-2 border-white border-opacity-80"
+              style={{ transform: 'scale(0.8)' }}
+            />
+            <span className="status f-online absolute bottom-2 right-1"></span>
+
+            <div className="user-setting text-center absolute right-14 top-[60px] w-40 bg-white shadow-lg  rounded-lg hidden group-hover:block z-10 items-center">
+              <ul className="log-out ">
+                <li className="py-1 hover:bg-gray-200 rounded-lg w-[100%]">
+                  <a
+                    href="about.html"
+                    className="text-gray-600 flex items-center h-8 pl-5"
+                  >
+                    <FontAwesomeIcon icon={faUser} className="mr-2" /> View
+                    Profile
+                  </a>
+                </li>
+                <li className="py-1 hover:bg-gray-200 rounded-lg pl-5">
+                  <a
+                    href="setting.html"
+                    className="text-gray-600 flex items-center h-8"
+                  >
+                    <FontAwesomeIcon icon={faPencilAlt} className="mr-2" /> Edit
+                    Profile
+                  </a>
+                </li>
+                <li className="py-1 hover:bg-gray-200 rounded-lg pl-5">
+                  <a
+                    href="logout.html"
+                    className="text-gray-600 flex items-center h-8"
+                  >
+                    <FontAwesomeIcon icon={faPowerOff} className="mr-2" /> Log
+                    out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
