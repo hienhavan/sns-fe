bcfe88d7dec75e48ef081424a7b6d2936b8818d8
trangnguyenhuu <nguyenhuutrang007@gmail.com>
2024-10-07 08:38:23 +0700

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import RegisterForm from './components/login/RegisterForm';
 import Home from './components/home/Home';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-        </Route>
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
