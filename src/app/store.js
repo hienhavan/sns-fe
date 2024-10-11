7a60075841fd258487827dc04af57e6d450414e4
keiv <vudangkhoi1994@gmail.com>
2024-10-12 00:08:55 +0700

    fix app store

--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/store/authSlice';
-import userReducer from '../features/post/store/postSlice';
-import postRducer from '../features/user/store/userSlice';
+import postRducer from '../features/post/store/postSlice';
+import userReducer from '../features/user/store/userSlice';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    users: userReducer,
-    posts: postRducer,
+    user: userReducer,
+    post: postRducer,
   },
 });
 
-store.subscribe(() => console.log(store.getState()));
+// store.subscribe(() => console.log(store.getState()));
 
 export default store;
