364984e0fbd172df0d40d91a7f081457dbb092d9
keiv <vudangkhoi1994@gmail.com>
2024-10-12 00:19:03 +0700

    fix feature post

--- src/features/post/components/PostList.jsx
+++ src/features/post/components/PostList.jsx
@@ -1,45 +1,42 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllPosts } from '../store/postSlice';
+import { getAllPosts } from '../services/post.js';
 import Post from './Post';
 import PostForm from './PostForm.jsx';
 
 const PostList = () => {
   const dispatch = useDispatch();
-
-  const { posts, isLoading, error } = useSelector((state) => state.user);
-
-  console.log("Posts in PostList component:", posts);
+  const { posts, isLoading, error } = useSelector((state) => state.post);
 
   useEffect(() => {
     dispatch(getAllPosts());
-  }, [dispatch]);
+  }, []);
 
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p>Loading...</p>;
+  // }
 
   if (error) {
     return <p>Error loading posts: {error}</p>;
   }
 
-  if (!posts || posts.length === 0) {
-    return <p>No posts available</p>;
-  }
-
   return (
     <div>
       <div>
-        <PostForm/>
+        <PostForm />
       </div>
-      <div>
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
-      </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {posts.length === 0 ? (
+            <p>No posts available</p>
+          ) : (
+            posts.map((post) => <Post key={post.id} post={post} />)
+          )}
+        </div>
+      )}
     </div>
-
   );
 };
 
