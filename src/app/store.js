bb9e7ab6da530418e0e922ef5d1d0d3e78b50f8f
keiv <vudangkhoi1994@gmail.com>
2024-10-14 16:53:29 +0700

    fix global store

--- src/app/store.js
+++ src/app/store.js
@@ -1,20 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { thunk } from 'redux-thunk';
 import authReducer from '../features/auth/store/authSlice';
 import postReducer from '../features/post/store/postSlice';
 import userReducer from '../features/user/store/userSlice';
-import commentReducer from '../features/comment/store/commentSlice'; // Đảm bảo rằng import đúng
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
     post: postReducer,
-    comment: commentReducer
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(thunk),
 });
-
 
 export default store;
