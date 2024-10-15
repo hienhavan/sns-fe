3f783c94dd6df40ce5a8d5861fdbd284b5be97b2
keiv <vudangkhoi1994@gmail.com>
2024-10-15 15:40:27 +0700

    add admin dashboard

--- src/features/admin/components/Dashboard.jsx
+++ src/features/admin/components/Dashboard.jsx
@@ -1,50 +1,33 @@
-import { Outlet } from 'react-router-dom';
 import ASideLeft from './ASideLeft';
-import BarChart from './BarChart';
-import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import AdminBarchart from './AdminBarChart';
+import { useState, useEffect } from 'react';
 import UserTable from './UserTable';
+import adminService from '../services/admin';
 
 const Dashboard = () => {
-  const rawData = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [data, setData] = useState();
 
-  const data = {
-    labels: rawData.map((d) => d.year),
-    datasets: [
-      {
-        label: 'My Chart',
-        data: rawData.map((d) => d.count),
-        backgroundColor: ['rgba(75,192,192,1)'],
-        borderColor: 'black',
-        borderWidth: 1,
-      },
-    ],
-  };
+  useEffect(() => {
+    (async () => {
+      const data = await adminService.getNewUserByYear(year);
+      setData(data);
+    })();
+  }, [year]);
 
-  const Layout = () => (
+  return (
     <div className="flex">
       <ASideLeft />
       <div className="flex h-screen w-full flex-col overflow-y-hidden">
         <div className="flex w-full flex-col overflow-x-hidden border-t">
           <main className="w-full flex-grow p-6">
-            {/* <Outlet /> */}
-            <BarChart data={data} />
+            <AdminBarchart data={data} />
             <UserTable />
           </main>
         </div>
       </div>
     </div>
   );
-
-  return <Layout />;
 };
 
 export default Dashboard;
--- src/features/admin/services/admin.js
+++ src/features/admin/services/admin.js
@@ -1,1 +1,34 @@
-import axios from 'axios';
+import axios from '../../../utils/axiosClient';
+
+const getNewUserByYear = async (year) => {
+  const response = await axios.get(
+    `/apihost/api/v1/users/new-users?year=${year}`,
+  );
+  return response.data;
+};
+
+const getNewUserByMonthAndYear = async (month, year) => {
+  const response = await axios.get(
+    `/apihost/api/v1/users/new-users?month=${month}&year=${year}`,
+  );
+  return response.data;
+};
+
+const filterUser = async (filter) => {
+  let q = '';
+  if (filter) q += `?name=${filter}`;
+  const response = await axios.get(`/apihost/api/v1/users${q}`);
+  return response.data;
+};
+
+const blockUser = async (user) => {
+  const response = await axios.put(`/apihost/api/v1/users/${user.id}/block`);
+  return response.data;
+};
+
+export default {
+  blockUser,
+  getNewUserByMonthAndYear,
+  getNewUserByYear,
+  filterUser,
+};
